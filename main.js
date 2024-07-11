d0708e7869e734a37174faa8fdf786f31c14cd18
Alejandro Cena <alejandro.cena@tiendanube.com>
2024-07-11 06:00:06 -0300

    pipes and filters enriched

--- main.js
+++ main.js
@@ -3,6 +3,35 @@ import processPipesFilters from './runtimes/pipes-and-filters.js';
 import processRxJs from './runtimes/rx-js.js';
 import processEventOriented from './runtimes/event-oriented.js';
 
+const logger = console;
 
-processPipesFilters(['uno','dos','tres','cuatro','cinco','seis'], 12345);
+
+const filterEvenStores = payload => {
+    if(payload.store % 2 == 0) {
+        return payload;
+    } else {
+        logger.info("Batch Discarted cause Odd Store");
+    }
+};
+
+const splitBatchIntoChunks = payload => {
+    const batches = [];
+    const chunkSize = 2;
+    for (let i = 0; i < payload.batches[0].length; i += chunkSize) {
+        const chunk = payload.batches[0].slice(i, i + chunkSize);
+        batches.push(chunk);
+    }
+    payload.batches = batches;
+    return payload;
+}
+
+const priorityQueueCriteria = payload => {
+    const storeString = payload.store+'';
+    if(Number(storeString[0]) % 2 == 0) {
+        payload.queue = 'piority'
+    }
+    return payload;
+}
+
+processPipesFilters(['uno','dos','tres','cuatro','cinco','seis'], 22346,[filterEvenStores],[splitBatchIntoChunks,priorityQueueCriteria]);
 
--- runtimes/pipes-and-filters.js
+++ runtimes/pipes-and-filters.js
@@ -2,30 +2,33 @@ import { publish } from "../publish-service.js";
 
 const DEFAULT_QUEUE = 'standard';
 
-const main_process = async (batch, store, batchReceivedFunnctions=[], batchPrePrublishFunctions=[]) => {
+const main_process = async (batch, store, batchReceivedFunctions=[], batchPrePrublishFunctions=[]) => {
     
     const initialBatchReceivedPayload = {
         batch,
         store
     };
-    const finalBatchReceived = batchReceivedFunnctions.reduce( (payload, operation ) => {
+
+    const finalBatchReceived = batchReceivedFunctions.reduce( (payload, operation ) => {
         return operation(payload)
     }, initialBatchReceivedPayload);
     
-    
-    const initialPublishPayload = {
-        batches:[finalBatchReceived.batch],
-        store: finalBatchReceived.store,
-        queue: DEFAULT_QUEUE
-    };
-    const finalPublishPayload = batchPrePrublishFunctions.reduce((payload, operation ) => {
-        const modifiedPayload = operation(payload);
-        return modifiedPayload;
-    }, initialPublishPayload)
+    if (finalBatchReceived) {
+        const initialPublishPayload = {
+            batches:[finalBatchReceived.batch],
+            store: finalBatchReceived.store,
+            queue: DEFAULT_QUEUE
+        };
 
-    console.log("RESULT", JSON.stringify(finalPublishPayload));
-    const publishingCalls = finalPublishPayload.batches.map( batch => publish(finalPublishPayload.topic, {store: finalPublishPayload.store, batch }));
-    await Promise.all(publishingCalls);
+        const finalPublishPayload = batchPrePrublishFunctions.reduce((payload, operation ) => {
+            const modifiedPayload = operation(payload);
+            return modifiedPayload;
+        }, initialPublishPayload)
+
+        console.log("RESULT", JSON.stringify(finalPublishPayload));
+        const publishingCalls = finalPublishPayload.batches.map( batch => publish(finalPublishPayload.queue, {store: finalPublishPayload.store, batch }));
+        await Promise.all(publishingCalls);
+    }
 };
 
 export default main_process;
